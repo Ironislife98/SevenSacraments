39d50974d13b4bb6c34be1b9073fd63eeccdbcad
EmilioMendoza <Ironislife39@gmail.com>
2021-03-31 01:58:19 -0400

    Done

--- main.py
+++ main.py
@@ -6,5 +6,32 @@ app = Flask(__name__)
 def home():
     return render_template("Index.html")
 
+@app.route('/baptism')
+def baptism():
+    return render_template("baptism.html")
+
+@app.route("/confirmation")
+def confirmation():
+    return render_template("confirmation.html")
+
+@app.route("/eucharist")
+def eucharist():
+    return render_template("eucharist.html")
+
+@app.route("/penance")
+def penance():
+    return render_template("penance.html")
+
+@app.route("/anointing")
+def anointing():
+    return render_template("anointing.html")
+
+@app.route("/holyorders")
+def holyorders():
+    return render_template("holyorders.html")
+
+@app.route('/marriage')
+def marriage():
+    return render_template("marriage.html")
 if __name__ == "__main__":
     app.run(debug=True)
